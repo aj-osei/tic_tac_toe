7fb3f5bc4a8e5b2bebc8dae8afc2153cb91bf67e
A.J. Osei <169005249+aj-osei@users.noreply.github.com>
2024-12-19 17:41:31 +0000

    set up my function prototypes

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 #include <ctype.h>
 #include <time.h>
 
+void resetBoard();          //reset the 2d character array
+void printBoard();          //used to print 2d charatcer array
+int checkFreeSpaces();      // check if there is any spaces to move
+void playerMove();          // for when its the player turn to move
+void computerMove();        // for when it's the computer turn to move
+char checkWinner();         // used to check who won
+void printWinner(char);         // used to print the player who won
+
+
 int main(){
-    return o;
+    return 0;
 }

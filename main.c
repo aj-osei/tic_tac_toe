f4fbc6ffd131b926284af01609e75a167d021eaa
A.J. Osei <169005249+aj-osei@users.noreply.github.com>
2024-12-19 19:08:50 +0000

    set up checkWinner function!

--- main.c
+++ main.c
@@ -106,7 +106,32 @@ void computerMove(){
 
 // used to check who won
 char checkWinner(){
+    // check rows
+    for (int i = 0; i < 3; i++){
+        if(board[i][0] == board[i][1] && board[i][0] == board[i][2]){
+            return board[i][0];
 
+        }
+
+    }
+    // check columns
+    for(int i = 0; i < 3; i++){
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i]){
+            return board [0][i];
+
+        }
+
+    }
+    //check diagonals
+    if(board[0][0] == board[1][1] && board[0][0] == board[2][2]){
+        return board [0][0];
+    }
+    // check other diagonal
+    if(board[0][2] == board[1][1] && board[0][2] == board[2][0]){
+        return board [0][2];
+    }
+
+    return ' ';
 } 
 
 // used to print the player who won

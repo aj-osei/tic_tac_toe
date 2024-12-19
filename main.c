d7e7bef5d87151550d6235d8f0d578457c9d7dde
A.J. Osei <169005249+aj-osei@users.noreply.github.com>
2024-12-19 17:44:35 +0000

    set up body for functions protoypes under main function

--- main.c
+++ main.c
@@ -15,3 +15,38 @@ void printWinner(char);         // used to print the player who won
 int main(){
     return 0;
 }
+
+//reset the 2d character array
+void resetBoard(){
+
+}  
+
+//used to print 2d charatcer array
+void printBoard(){
+
+}
+
+// check if there is any spaces to move
+int checkFreeSpaces(){
+
+}
+
+// for when its the player turn to move
+void playerMove(){
+
+} 
+
+// for when it's the computer turn to move
+void computerMove(){
+
+}  
+
+// used to check who won
+char checkWinner(){
+
+} 
+
+// used to print the player who won
+void printWinner(char winner){
+
+}         
